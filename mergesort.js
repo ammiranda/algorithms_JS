665aba5e9cb0e6a699a3f3c41d9a8f0598077164
Alexander <alexandermichaelmiranda@gmail.com>
2013-12-14 19:57:26 -0800

    Added comments

--- mergesort.js
+++ mergesort.js
@@ -1,16 +1,16 @@
 function mergeSort(array) {
-	if (array.length < 2){
+	if (array.length < 2){  // Check for trivial array input
 		return array;
 	}
-	var middle = Math.floor(array.length / 2);
-	var left = array.slice(0, middle);
-	var right = array.slice(middle);
+	var middle = Math.floor(array.length / 2); // Find length of half of the inputted array
+	var left = array.slice(0, middle);  // Assign the left half of array to var left
+	var right = array.slice(middle);    // Assign the right half of array to var right
 
-	var leftSorted = mergeSort(left);
-	var rightSorted = mergeSort(right);
+	var leftSorted = mergeSort(left);   // Recursively called mergeSort on both right and left halves
+	var rightSorted = mergeSort(right);  
 	var sorted = [];
 
-	while (leftSorted.length > 0 || rightSorted.length > 0) {
+	while (leftSorted.length > 0 || rightSorted.length > 0) {  // Continue iteration until one of the sorted arrays is emptied
 		if (leftSorted.length < 1) {
 			sorted = sorted.concat(rightSorted);
 			break;
@@ -20,15 +20,15 @@ function mergeSort(array) {
 			break;
 		}
 		if (leftSorted[0] < rightSorted[0]) {
-			var num = leftSorted.shift();
+			var num = leftSorted.shift();  // Remove first number from array and pass it into var num
 		}
 		else {
 			var num = rightSorted.shift();
 		}
-		sorted.push(num);
+		sorted.push(num);  // Append num to end of sorted array
 	}
-	return sorted;
+	return sorted;  // Output the sorted array after all of the numbers have been evaluated
 
 }
 
-console.log(mergeSort([1,4,445,3,8,12,7]));
+console.log(mergeSort([1,4,445,3,8,12,7]));  // Test call
