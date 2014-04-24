34a91de2ef9dc335c0c4682068cabeeaf0a3fc68
Alexander <alexandermichaelmiranda@gmail.com>
2014-04-23 17:42:21 -0700

    Added sieve of eratosthenes implementation in JavaScript

---
+++ javascript/sieve.js
@@ -0,0 +1,25 @@
+var eratosthenes = function(n) {
+	var array = [], upperLimit = Math.sqrt(n), output = [];
+
+	for (var i = 0; i < n; i++) {
+		array.push(true);	
+	}
+
+	for (var i = 2; i <= upperLimit; i++) {
+		if (array[i]) {
+			for (var j = i * i; j < n; j += i) {
+				array[j] = false;
+			}
+		}
+	}
+
+	for (var i = 2; i < n; i++) {
+		if (array[i]) {
+			output.push(i);
+		}
+	}
+
+	return output;
+};
+
+console.log(eratosthenes(10));
